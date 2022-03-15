b49def08650aff198b6acae83407ed773faac34e
Christian Veelken <christian.veelken@cern.ch>
2022-03-15 17:44:29 +0200

    added support for version and added QCD sample

--- test/produceMiniAOD.sh
+++ test/produceMiniAOD.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/sh
 
-cmsDriver.py produceTallinnTausMiniAOD --filein file:produceTallinnTaus_AODSIM.root --fileout file:produceTallinnTaus_MINIAODSIM.root --mc --eventcontent MINIAODSIM --datatier MINIAODSIM --conditions auto:phase1_2021_realistic --step PAT --nThreads 1 --geometry DB:Extended --era Run3 --python_filename produceTallinnTaus_MiniAOD_cfg.py --no_exec --customise TallinnTauTag/RecoTau/tools/customizeMiniAOD.customizeTallinnTausMiniAOD -n -1
-rm produceTallinnTaus_MiniAOD.log
-cmsRun produceTallinnTaus_MiniAOD_cfg.py >& produceTallinnTaus_MiniAOD.log
+version=$1
+
+cmsDriver.py produceTallinnTausMiniAOD --filein "file:produceTallinnTaus_ggH_htt_DNN_$version_AODSIM.root" --fileout "file:produceTallinnTaus_ggH_htt_DNN_$version_MINIAODSIM.root" --mc --eventcontent MINIAODSIM --datatier MINIAODSIM --conditions auto:phase1_2021_realistic --step PAT --nThreads 1 --geometry DB:Extended --era Run3 --python_filename produceTallinnTaus_ggH_htt_DNN_MiniAOD_cfg.py --no_exec --customise TallinnTauTag/RecoTau/tools/customizeMiniAOD.customizeTallinnTausMiniAOD -n -1
+rm produceTallinnTaus_ggH_htt_DNN_MiniAOD.log
+cmsRun produceTallinnTaus_ggH_htt_DNN_MiniAOD_cfg.py >& produceTallinnTaus_ggH_htt_DNN_MiniAOD.log
+
+cmsDriver.py produceTallinnTausMiniAOD --filein "file:produceTallinnTaus_qcd_DNN_$version_AODSIM.root" --fileout "file:produceTallinnTaus_qcd_DNN_$version_MINIAODSIM.root" --mc --eventcontent MINIAODSIM --datatier MINIAODSIM --conditions auto:phase1_2021_realistic --step PAT --nThreads 1 --geometry DB:Extended --era Run3 --python_filename produceTallinnTaus_qcd_DNN_MiniAOD_cfg.py --no_exec --customise TallinnTauTag/RecoTau/tools/customizeMiniAOD.customizeTallinnTausMiniAOD -n -1
+rm produceTallinnTaus_qcd_DNN_MiniAOD.log
+cmsRun produceTallinnTaus_qcd_DNN_MiniAOD_cfg.py >& produceTallinnTaus_qcd_DNN_MiniAOD.log
+
+
--- test/produceNanoAOD.sh
+++ test/produceNanoAOD.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/sh
 
-cmsDriver.py produceTallinnTausNANOAOD --filein file:produceTallinnTaus_MINIAODSIM.root --fileout file:produceTallinnTaus_NANOAODSIM.root --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions auto:phase1_2021_realistic --step NANO --nThreads 1 --era Run3 --python_filename produceTallinnTaus_nanoAOD_cfg.py --no_exec --customise TallinnTauTag/RecoTau/tools/customizeNanoAOD.customizeTallinnTausNanoAOD -n -1
-rm produceTallinnTaus_nanoAOD.log
-cmsRun produceTallinnTaus_nanoAOD_cfg.py >& produceTallinnTaus_nanoAOD.log
+cmsDriver.py produceTallinnTausNANOAOD --filein "file:produceTallinnTaus_ggH_htt_DNN_$version_MINIAODSIM.root" --fileout "file:produceTallinnTaus_ggH_htt_DNN_$version_NANOAODSIM.root" --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions auto:phase1_2021_realistic --step NANO --nThreads 1 --era Run3 --python_filename produceTallinnTaus_ggH_htt_DNN_nanoAOD_cfg.py --no_exec --customise TallinnTauTag/RecoTau/tools/customizeNanoAOD.customizeTallinnTausNanoAOD -n -1
+rm produceTallinnTaus_ggH_htt_DNN_nanoAOD.log
+cmsRun produceTallinnTaus_ggH_htt_DNN_nanoAOD_cfg.py >& produceTallinnTaus_ggH_htt_DNN_nanoAOD.log
+
+cmsDriver.py produceTallinnTausNANOAOD --filein "file:produceTallinnTaus_qcd_DNN_$version_MINIAODSIM.root" --fileout "file:produceTallinnTaus_qcd_DNN_$version_NANOAODSIM.root" --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions auto:phase1_2021_realistic --step NANO --nThreads 1 --era Run3 --python_filename produceTallinnTaus_qcd_DNN_nanoAOD_cfg.py --no_exec --customise TallinnTauTag/RecoTau/tools/customizeNanoAOD.customizeTallinnTausNanoAOD -n -1
+rm produceTallinnTaus_qcd_DNN_nanoAOD.log
+cmsRun produceTallinnTaus_qcd_DNN_nanoAOD_cfg.py >& produceTallinnTaus_qcd_DNN_nanoAOD.log
+
